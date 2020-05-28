a57fc2a068e36ad452193568631958045cf8f453
003300 <xiehaifan@fcbox.com>
2020-05-28 19:32:49 +0800

    分布式锁

--- netty-mqtt-broker/src/main/java/com/github/netty/mqtt/broker/lock/Locker.java
+++ netty-mqtt-broker/src/main/java/com/github/netty/mqtt/broker/lock/Locker.java
@@ -1,18 +1,25 @@
 package com.github.netty.mqtt.broker.lock;
 
+import io.lettuce.core.internal.LettuceLists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.jedis.JedisClusterConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 
-import java.util.Objects;
+import java.util.*;
 
 /**
  * 类描述:
- *  https://blog.csdn.net/long2010110/article/details/82911168
+ * https://blog.csdn.net/long2010110/article/details/82911168
+ *
  * @author 003300
  * @version 1.0
  * @date 2020/5/21 14:44
@@ -24,44 +31,67 @@ public class Locker {
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
+    private static final Long SUCCESS = 1L;
+
+    private static final String LOCK_PREFIX = "redis:lock:";
+
+    private static final int LOCK_EXPIRE = 300;
+
     public void lock(String key, Perform perform) {
         try {
-            if (lock(key)) {
+            if (lock(key, "1", LOCK_EXPIRE)) {
                 perform.perform();
             }
         } catch (Exception e) {
             log.error("加锁发生异常");
         } finally {
-            delete(key);
+            release(key);
         }
     }
 
-    private static final String LOCK_PREFIX = "redis:lock:";
-    private static final int LOCK_EXPIRE = 300;
-    private boolean lock(String key) {
-        String lock = LOCK_PREFIX + key;
-        return (Boolean) redisTemplate.execute((RedisCallback) connection -> {
-            long expireAt = System.currentTimeMillis() + LOCK_EXPIRE;
-            Boolean acquire = connection.setNX(lock.getBytes(), String.valueOf(expireAt).getBytes());
-            if (acquire) {
-                return true;
-            } else {
-                byte[] value = connection.get(lock.getBytes());
-                if (Objects.nonNull(value) && value.length > 0) {
-                    long expireTime = Long.parseLong(new String(value));
-                    // 如果锁已经过期
-                    if (expireTime < System.currentTimeMillis()) {
-                        // 重新加锁
-                        byte[] oldValue = connection.getSet(lock.getBytes(), String.valueOf(System.currentTimeMillis() + LOCK_EXPIRE).getBytes());
-                        return Long.parseLong(new String(oldValue)) < System.currentTimeMillis();
-                    }
+    private boolean lock(String key, String value, int expire) {
+        try {
+            String script = "if redis.call('setNx',KEYS[1],ARGV[1]) == 1 " +
+                    "then " +
+                    "if redis.call('get',KEYS[1])==ARGV[1] " +
+                    "then " +
+                    "return redis.call('expire',KEYS[1],ARGV[2]) " +
+                    "else " +
+                    "return 0 " +
+                    "end " +
+                    "end";
+            List<String> args = Arrays.asList(value, String.valueOf(expire));
+            Long result = redisTemplate.execute((RedisCallback<Long>) connection -> {
+                Object nativeConnection = connection.getNativeConnection();
+                if (nativeConnection instanceof JedisCluster) {
+                    return (Long) ((JedisCluster) nativeConnection).eval(script, Collections.singletonList(LOCK_PREFIX + key), args);
+                } else if (nativeConnection instanceof Jedis) {
+                    return (Long) ((Jedis) nativeConnection).eval(script, Collections.singletonList(LOCK_PREFIX + key), args);
                 }
+                return null;
+            });
+            log.info("LOCK -- key: {} result: {}", key, result);
+            if (SUCCESS.equals(result)) {
+                return true;
             }
-            return false;
-        });
+        } catch (Exception e) {
+            log.error("locker exception", e);
+        }
+        return false;
     }
 
-    private void delete(String key) {
-        redisTemplate.delete(key);
+    private void release(String key) {
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        List<String> args = LettuceLists.newList();
+        Long result = redisTemplate.execute((RedisCallback<Long>) connection -> {
+            Object nativeConnection = connection.getNativeConnection();
+            if (nativeConnection instanceof JedisCluster) {
+                return (Long) ((JedisCluster) nativeConnection).eval(script, Collections.singletonList(LOCK_PREFIX + key), args);
+            } else if (nativeConnection instanceof Jedis) {
+                return (Long) ((Jedis) nativeConnection).eval(script, Collections.singletonList(LOCK_PREFIX + key), args);
+            }
+            return null;
+        });
+        log.info("RELEASE -- key: {} result: {}", key, result);
     }
 }
